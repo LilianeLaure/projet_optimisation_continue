02563c3017a3dd6b0d246683d38133e1b0e73d63
Lgaudelet <lucasgaudelet@gmail.com>
2017-01-16 14:22:08 +0100

    modificatoin de la génération des dat

--- mat/dat_generation.m
+++
@@ -1,5 +0,0 @@
-src_file = 'dat/1/a05100';
-dest_file = 'dat/a05100.dat';
-
-[C, A, b] = loadfile(src_file);
-createDatFileP(dest_file, C, A, b);
---
+++ mat/dat_generator.m
@@ -0,0 +1,9 @@
+function [] = dat_generator( src_file, dest_file )
+
+% src_file = 'dat/1/a05100';
+% dest_file = 'dat/a05100.dat';
+
+[C, A, b] = loadfile(src_file);
+createDatFileP(dest_file, C, A, b);
+
+end
