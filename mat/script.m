8d265a61c9a1413e33f58855e3d2549b0171d097
grosjean1 <user@user-VirtualBox.(none)>
2017-01-19 22:27:45 +0100

    question 1partie B

--- mat/script.m
+++ mat/script.m
@@ -1,5 +1,4 @@
-%PAS FINI 
-
+%PATTERNSEARCH et GA
 %On prend les valeurs 
 src_file = '../dat/1/a05100';
 [C,A,b] = loadfile(src_file);
@@ -27,6 +26,23 @@ beq=ones(n,1);
 lb = zeros(m*n,1);
 ub = ones(m*n,1);
 X0=zeros(m*n,1);
-x = patternsearch(fun,X0,B,b,Aeq,beq,lb,ub,@mycon)
-
-% x = ga(fitnessfcn,nvars)
+mycon=@nonlinear;
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%PATTERNSEARCH
+%option=psoptimset('TolCon',1e-6);
+%option = psoptimset('TolMesh',0.02,'ScaleMesh','off');
+x = patternsearch(fun,X0,B,b,Aeq,beq,lb,ub,mycon);
+ SolPatternSearch=zeros(m,n);
+ SolPatternSearch(1,:)=x(1:n)';
+ for j=2:m
+     SolPatternSearch(j,:)=x(n*(j-1)+1:n*j);
+ end
+ SolPatternSearch
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%GA
+IntCon=[1:m*n];
+x2 = ga(fun,500,B,b,[],[],lb,ub,@nonlinearga,IntCon);
+ SolGa=zeros(m,n);
+ SolGa(1,:)=x2(1:n)';
+ for j=2:m
+     SolGa(j,:)=x2(n*(j-1)+1:n*j);
+ end
+ SolGa
